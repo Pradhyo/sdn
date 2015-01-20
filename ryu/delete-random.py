c8518f0208502fd687c54ca580ec51f1560b60ba
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-20 17:34:48 +0530

    Add file to delete random entry upon overflow

---
+++ ryu/delete-random.py
@@ -0,0 +1,126 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet, ipv4, tcp
+from random import randint
+from collections import deque
+
+max = 5
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        #srcport followed my dstport are stored upto 'max' values
+        self.storedports = deque((0,0), max)
+        self.storedports.pop()
+        self.storedports.pop()
+        self.totalrules = 1 #to count the table-miss flow entry
+        #So switch will have a maximum of max flows including the table-miss entry
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    def del_flow(self, datapath, src_port, dst_port):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        print "Deleting a random flow entry"
+        match = parser.OFPMatch(eth_type=0x800, ip_proto=6, tcp_src=src_port, tcp_dst=dst_port)
+        mod = parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_DELETE)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+        
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
+        pkt_tcp = pkt.get_protocol(tcp.tcp)
+
+        if pkt_tcp:
+            src_port = pkt_tcp.src_port
+            dst_port = pkt_tcp.dst_port
+
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            self.totalrules += 1
+            print self.storedports #to print the fifo queue stored by controller
+            if pkt_tcp:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_type=0x800, ip_proto=6, tcp_src=src_port, tcp_dst=dst_port)
+                if self.totalrules <= max:
+                    self.add_flow(datapath, 2, match, actions)
+                else:
+                    temp_random = randint(1,max)
+                    self.storedports.rotate(temp_random)
+                    temp = self.storedports.pop()
+                    #self.storedports.rotate(-temp_random)
+                    temp_src = temp[0]
+                    temp_dst = temp[1]
+                    self.del_flow(datapath,temp_src,temp_dst)
+                    
+                self.storedports.appendleft((src_port,dst_port))
+                
+                    
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
